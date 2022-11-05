a6c2679c3f558a7d93ee91c00484ac3fd577d043
vasily-loskutov <vasilijloskutov2083@gmail.com>
2022-11-05 13:36:29 +0300

    aim training

---
+++ aim training/main.js
@@ -0,0 +1,66 @@
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+let score = 0;
+var time = 0;
+startBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    screens[0].classList.add('up')
+})  
+timeList.addEventListener('click',(event)=>{
+    if(event.target.classList.contains('time-btn')){
+        time = parseInt(event.target.dataset.time) 
+       screens[1].classList.add('up')
+       startGame();
+    }
+})
+board.addEventListener('click',(event)=>{
+    if(event.target.classList.contains('circle')){
+        score++;
+        event.target.remove('circle')
+        createRandomCircle()
+    }
+})
+function startGame(){
+    
+    setInterval(decreaseTime,1000)
+    createRandomCircle()
+   
+}
+function decreaseTime(){
+    if(time === 0){
+       finishGame()
+    }else{
+
+   
+    let current = --time;
+    if(current < 10){
+        current = `0${current}`
+    }
+    timeEl.innerHTML = `00:${current}`
+    }
+}
+function finishGame(){
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Cчёт:<span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle(){
+    const size = getRandomNumber(10,60)
+    const {width,height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0,width - size)
+    const y = getRandomNumber(0,height - size)
+    const circle = document.createElement('div')
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+    circle.style.background = chroma.random()
+    board.append(circle)
+}
+function getRandomNumber(min,max){
+   return Math.round(Math.random() * (max-min) + min)
+}
